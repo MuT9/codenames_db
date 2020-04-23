bac51ef66ee19d22554edd33761355b929e92617
Titov Dmitry <dktitov96@gmail.com>
2020-04-23 20:47:07 +0200

    fix delete routes

--- routes/games.js
+++ routes/games.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const Game = require('../models/Game');
+const Room = require('../models/Room');
 
 const gamesRouter = new Router();
 
@@ -11,9 +12,19 @@ const generateAnswer = (success = false, res = null, err = null) => ({
 });
 
 gamesRouter.route('/')
+    .get(async (req, res) => {
+        try {
+            const games = await Game.find().lean();
+
+            res.json(generateAnswer(true, games));
+        } catch (e) {
+            res.json(generateAnswer(false, undefined, { message: e.message }))
+        }
+    })
     .delete(async (req, res) => {
         try {
             await Game.deleteOne({ chat_id: req.body.chat_id });
+            await Room.updateOne({ chat_id: req.body.chat_id }, { $set: { in_game: false } })
 
             res.json(generateAnswer(true));
         } catch (e) {
